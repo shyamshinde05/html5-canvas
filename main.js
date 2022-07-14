50a8240b01a999223eea736a5d014f87d43875d8
shyam <shyams@alohatechnology.com>
2022-07-14 12:26:06 +0530

    more updates

--- main.js
+++ main.js
@@ -4,15 +4,23 @@ const context = canvas.getContext("2d");
 let scale = 1.0;
 let scaleMultiplier = 0.8;
 let isMouseDown = false;
+
 let isDragging = false;
+let dragged = false;
+
 let startDragOffset = {};
 
 let startX;
 let startY;
 
+let allowPanning = false;
+let currentCardIndex;
+
 const translatePosition = {
-    x: canvas.width / 2,
-    y: canvas.height / 2
+    // x: canvas.width / 2,
+    // y: canvas.height / 2
+    x: 0,
+    y: 0
 };
 
 const zoomIn = () => {
@@ -25,8 +33,13 @@ const zoomOut = () => {
     drawCards(scale, translatePosition);
 }
 
+const togglePanMode = () => {
+    allowPanning = !allowPanning;
+}
+
 document.getElementById("plus").addEventListener("click", zoomIn, false);
 document.getElementById("minus").addEventListener("click", zoomOut, false);
+document.getElementById("pan-mode").addEventListener("click", togglePanMode, false);
 
 // set the canvas to the size of the window
 canvas.width = window.innerWidth - 20;
@@ -54,15 +67,37 @@ const cards = [
 ];
 
 const handleMouseDown = (e) => {
+
+    console.log('mouse down');
+
     isMouseDown = true;
     isDragging = false;
-    
+    dragged = false;
+
+    // startX = e.clientX;
+    // startY = e.clientY;
+
+    startX = (e.clientX - translatePosition.x) / scale;
+    startY = (e.clientY - translatePosition.y) / scale;
+
     startDragOffset.x = e.clientX - translatePosition.x;
     startDragOffset.y = e.clientY - translatePosition.y;
+
+    const x = (e.clientX - translatePosition.x) / scale;
+    const y = (e.clientY - translatePosition.y) / scale;
+
+    const index = cards.findIndex(c => x > c.x && x < (c.x + c.width) && y > c.y && y < (c.y + c.height));
+
+    if (index !== -1) {
+        isDragging = true;
+        currentCardIndex = index;
+    }
 }
 
 const handleMouseUp = (e) => {
+    console.log('mouse up');
     isMouseDown = false;
+    isDragging = false;
 }
 
 const handleMouseover = (e) => {
@@ -71,19 +106,53 @@ const handleMouseover = (e) => {
 
 const handleMouseout = (e) => {
     isMouseDown = false;
+    isDragging = false;
 }
 
 const handleMousemove = (e) => {
-    isDragging = isMouseDown;
-    if (isMouseDown) {
-        translatePosition.x = e.clientX - startDragOffset.x;
-        translatePosition.y = e.clientY - startDragOffset.y;
-        drawCards(scale, translatePosition);
+    if (isMouseDown && allowPanning) {
+        panCanvas(e);
+        return;
     }
+
+    if (isDragging && currentCardIndex > -1) {
+
+        // const mouseX = e.clientX;
+        // const mouseY = e.clientY;
+
+        const mouseX = (e.clientX - translatePosition.x) / scale;
+        const mouseY = (e.clientY - translatePosition.y) / scale;
+
+        const dx = mouseX - startX;
+        const dy = mouseY - startY;
+
+        const card = cards[currentCardIndex];
+
+        card.x += dx;
+        card.y += dy;
+
+        drawCards(scale, translatePosition);
+
+        startX = mouseX;
+        startY = mouseY;
+
+        dragged = true;
+
+        return;
+
+    }
+
+}
+
+const panCanvas = (e) => {
+    translatePosition.x = e.clientX - startDragOffset.x;
+    translatePosition.y = e.clientY - startDragOffset.y;
+    drawCards(scale, translatePosition);
 }
 
 const handleClick = (e) => {
-    if (isDragging) {
+    console.log('mouse click');
+    if (dragged || allowPanning) {
         return;
     }
 
@@ -94,7 +163,7 @@ const handleClick = (e) => {
     console.log("scale", scale);
 
     const index = cards.findIndex(c => x > c.x && x < (c.x + c.width) && y > c.y && y < (c.y + c.height));
-    debugger;
+
     if (index > -1) {
         const color = cards[index].color;
         alert('clicked  ' + color);
